9282bd7d7d27fedecedd5d88ae76d9bde8633cf6
Simon <simon@bitcartel.com>
2016-01-18 14:37:59 -0800

    update

--- test.sh
+++ test.sh
@@ -27,8 +27,8 @@ cp multichain-1.0-alpha*/multichain* /usr/local/bin/
 
 # As regular user
 su -l $AZUREUSER -c "multichain-util create ${CHAINNAME}"
-su -l $AZUREUSER -c "sed -i \"s/^default-network-port =.*/default-network-port = 8333/\" $HOMEDIR/.multichain/${CHAINNAME}/params.dat"
-su -l $AZUREUSER -c "sed -i \"s/^default-rpc-port =.*/default-rpc-port = 8332/\" $HOMEDIR/.multichain/${CHAINNAME}/params.dat"
+su -l $AZUREUSER -c "sed -i \"s/^default-network-port =.*/default-network-port = 4333/\" $HOMEDIR/.multichain/${CHAINNAME}/params.dat"
+su -l $AZUREUSER -c "sed -i \"s/^default-rpc-port =.*/default-rpc-port = 4332/\" $HOMEDIR/.multichain/${CHAINNAME}/params.dat"
 su -l $AZUREUSER -c "multichaind ${CHAINNAME} -daemon"
 sleep 5
 su -l $AZUREUSER -c "multichain-cli ${CHAINNAME} getinfo"
